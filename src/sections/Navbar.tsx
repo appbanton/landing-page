551a9f2fb425337299a0f529c7aba3bc67ef0e94
Joe <theappbanton@gmail.com>
2025-02-05 18:18:46 -0400

    bug fix

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -119,7 +119,7 @@ export default function Navbar() {
                       className="flex items-center"
                       href="https://forms.gle/z5g59bT6x1NaZnNv5"
                     >
-                      Let's chat
+                      Let&apos;s chat
                     </Button>
                   </div>
                 </motion.div>
